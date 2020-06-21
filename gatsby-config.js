0b82b4467560bd42dbdb5d8e9f8d4b7c08d18bff
Diego Kfuri <kkfuri@gmail.com>
2020-06-20 21:33:37 -0300

    feat: realize helmet and icons setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,10 +2,12 @@ module.exports = {
   siteMetadata: {
     title: "anime por fredinho",
     description: "aqui falo sobre tudo que tem de pior",
+    author: "Fred Zampier",
   },
   plugins: [
     "gatsby-plugin-postcss",
     "gatsby-transformer-remark",
+    "gatsby-plugin-react-helmet",
     {
       resolve: "gatsby-source-filesystem",
       options: { name: "pages", path: `${__dirname}/src/content` },
