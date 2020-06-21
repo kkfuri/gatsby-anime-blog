bcab1b4621b9fda5d8dcee6a4fd4727ce6ddf51d
Diego Kfuri <kkfuri@gmail.com>
2020-06-20 21:35:55 -0300

    feat: remove console.logs and add check if arrays exhist

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,23 +5,21 @@ import Layout from "../components/layout/"
 import Post from "../components/post/"
 
 function Home({ data }) {
-  console.log(data)
   const [firstEdge, ...rest] = data.allMarkdownRemark.edges
-
-  console.log(firstEdge)
-  console.log(rest)
   return (
     <Layout>
       <div className="grid grid-cols-1 gap-4 my-20 xl:gap-8 xl:grid-cols-6">
-        <div className="mb-8 xl:grid xl:col-span-4 xl:grid-rows-1 xl:row-span-2 xl:mb-0">
-          <Post
-            featured
-            showImg
-            key={firstEdge.node.id}
-            {...firstEdge.node.frontmatter}
-          />
-        </div>
-        {rest.map((edge, index) => {
+        {firstEdge && (
+          <div className="mb-8 xl:grid xl:col-span-4 xl:grid-rows-1 xl:row-span-2 xl:mb-0">
+            <Post
+              featured
+              showImg
+              key={firstEdge.node.id}
+              {...firstEdge.node.frontmatter}
+            />
+          </div>
+        )}
+        {rest?.map((edge, index) => {
           const { id, frontmatter } = edge.node
           return (
             <div className="col-span-1 xl:col-span-2">
