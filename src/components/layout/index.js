699e316ee42b2f9fd44149aafc90e187bdc50602
Diego Kfuri <kkfuri@gmail.com>
2020-06-20 21:35:22 -0300

    feat: add SEO to layout

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -2,17 +2,19 @@ import React from "react"
 import { StaticQuery, graphql } from "gatsby"
 
 import Header from "../header"
+import SEO from "../seo"
 
-function Layout({ children, ...props }) {
+function Layout({ children, minimalist, ...props }) {
   return (
     <div className="container px-4 mx-auto my-12 full-w">
+      <SEO {...props} />
       <StaticQuery
         query={query}
         render={data => (
           <Header
             title={data.site.siteMetadata.title}
             description={data.site.siteMetadata.description}
-            {...props}
+            minimalist={minimalist}
           />
         )}
       />
