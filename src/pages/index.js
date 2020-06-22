42288c812846a18a250f757071be83a0a9bff997
Diego Kfuri <kkfuri@gmail.com>
2020-06-21 22:05:02 -0300

    feat: add empty state and create logic for repeating grid between every 9 posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,36 +1,40 @@
 import React from "react"
+import classnames from "classnames"
+import { FaSadCry } from "react-icons/fa"
 import { graphql } from "gatsby"
 
 import Layout from "../components/layout/"
 import Post from "../components/post/"
 
 function Home({ data }) {
-  const [firstEdge, ...rest] = data.allMarkdownRemark.edges
+  const posts = data.allMarkdownRemark.edges
   return (
     <Layout>
       <div className="grid grid-cols-1 gap-4 my-20 xl:gap-8 xl:grid-cols-6">
-        {firstEdge && (
-          <div className="mb-8 xl:grid xl:col-span-4 xl:grid-rows-1 xl:row-span-2 xl:mb-0">
-            <Post
-              featured
-              showImg
-              key={firstEdge.node.id}
-              {...firstEdge.node.frontmatter}
-            />
-          </div>
-        )}
-        {rest?.map((edge, index) => {
+        {posts?.map((edge, index) => {
           const { id, frontmatter } = edge.node
+          const featuredPost = index % 9 === 0
           return (
-            <div className="col-span-1 xl:col-span-2">
+            <div
+              className={classnames({
+                "mb-8 xl:grid xl:col-span-4 xl:grid-rows-1 xl:row-span-2 xl:mb-0": featuredPost,
+                "xl:col-span-2": index % 9 !== 0,
+              })}
+            >
               <Post
+                featured={featuredPost}
                 key={id}
                 {...frontmatter}
-                showImg={[0, 1].includes(index)}
+                showImg={[1, 2].includes(index % 9) || featuredPost}
               />
             </div>
           )
         })}
+        {posts?.length === 0 && (
+          <h1 className="col-span-6 text-6xl inline text-center font-display font-light">
+            Sem posts por enquanto <FaSadCry className="inline text-blue-500" />
+          </h1>
+        )}
       </div>
     </Layout>
   )
