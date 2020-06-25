13679326f457c4276f44813499089cddcc9488ea
Diego Kfuri <kkfuri@gmail.com>
2020-06-25 02:18:13 -0300

    fix: adjust style between mobile and desktop, add new animation for mouse users

--- src/components/navigatePost/index.js
+++ src/components/navigatePost/index.js
@@ -5,23 +5,31 @@ import { BsArrowRight, BsArrowLeft } from "react-icons/bs"
 const NavigatePost = ({ title, slug, image, prev, next }) => {
   return (
     <Link href={slug} className="group hover:text-primary">
-      {next && (
-        <span className="flex items-center space-x-4 justify-center mb-4">
-          <p>Ir para post mais antigo</p> <BsArrowRight />
+      <div className="transform hover:scale-110 duration-150">
+        <span className="flex items-center space-x-4 justify-center mb-2 text-gray-900">
+          {next && (
+            <>
+              <p>post mais antigo</p>
+              <BsArrowRight />
+            </>
+          )}
+          {prev && (
+            <>
+              <BsArrowLeft />
+              <p>post mais recente</p>
+            </>
+          )}
         </span>
-      )}
-      {prev && (
-        <span className="flex items-center space-x-4 justify-center mb-4">
-          <BsArrowLeft /> <p>Ir para post mais recente</p>
-        </span>
-      )}
-      <div className="p-4 rounded shadow cursor-pointer max-w-xs">
-        <img
-          src={image}
-          className="rounded"
-          alt={`Ir para página do post ${title}`}
-        />
-        <h4 className="text-2xl">{title}</h4>
+        <div className="p-4 rounded cursor-pointer max-w-xs">
+          <img
+            src={image}
+            className="rounded"
+            alt={`Ir para página do post ${title}`}
+          />
+          <h4 className="hidden md:block md:text-xl lg:text-2xl font-display text-center">
+            {title}
+          </h4>
+        </div>
       </div>
     </Link>
   )
