90b113994e2ea2ad5da151b42e94aa32777b64b4
Diego Kfuri <kkfuri@gmail.com>
2020-06-27 15:00:51 -0300

    feat: create function for slugify tags

---
+++ src/utils/slugify.js
@@ -0,0 +1,20 @@
+export function stringToSlug(str) {
+  str = str.replace(/^\s+|\s+$/g, "") // trim
+  str = str.toLowerCase()
+
+  // remove accents, swap ñ for n, etc
+  var from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;"
+  var to = "aaaaeeeeiiiioooouuuunc------"
+  for (var i = 0, l = from.length; i < l; i++) {
+    str = str.replace(new RegExp(from.charAt(i), "g"), to.charAt(i))
+  }
+
+  str = str
+    .replace(/[^a-z0-9 -]/g, "")
+    .replace(/\s+/g, "-")
+    .replace(/-+/g, "-")
+
+  return str
+}
+
+export default stringToSlug
