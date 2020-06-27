f29e9f27944310f609e9cafc495a43f690c46edd
Diego Kfuri <kkfuri@gmail.com>
2020-06-27 14:58:49 -0300

    feat: create page for tags

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,28 @@
 const path = require("path")
 
+function stringToSlug(str) {
+  str = str.replace(/^\s+|\s+$/g, "") // trim
+  str = str.toLowerCase()
+
+  // remove accents, swap ñ for n, etc
+  var from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;"
+  var to = "aaaaeeeeiiiioooouuuunc------"
+  for (var i = 0, l = from.length; i < l; i++) {
+    str = str.replace(new RegExp(from.charAt(i), "g"), to.charAt(i))
+  }
+
+  str = str
+    .replace(/[^a-z0-9 -]/g, "")
+    .replace(/\s+/g, "-")
+    .replace(/-+/g, "-")
+
+  return str
+}
+
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
-
   const blogPostTemplate = path.resolve("src/templates/blogPost.js")
+  const singleTagTemplate = path.resolve("src/templates/tagPage.js")
 
   return graphql(`
     query {
@@ -14,6 +33,8 @@ exports.createPages = ({ graphql, actions }) => {
               slug
               title
               image
+              tags
+              excerpt
             }
           }
         }
@@ -24,7 +45,16 @@ exports.createPages = ({ graphql, actions }) => {
       throw result.errors
     }
     const posts = result.data.allMarkdownRemark.edges
+    const postsByTag = {}
     posts.forEach(({ node }, index) => {
+      if (node.frontmatter.tags) {
+        node.frontmatter.tags.forEach(tag => {
+          if (!postsByTag[tag]) {
+            postsByTag[tag] = []
+          }
+          postsByTag[tag].push(node)
+        })
+      }
       createPage({
         path: `${node.frontmatter.slug}`,
         component: blogPostTemplate,
@@ -33,6 +63,21 @@ exports.createPages = ({ graphql, actions }) => {
           pathSlug: node.frontmatter.slug,
           prev: index === 0 ? null : posts[index - 1].node,
           next: index === posts.length - 1 ? null : posts[index + 1].node,
+        },
+      })
+    })
+    const tags = Object.keys(postsByTag)
+
+    tags.forEach(tagName => {
+      const slug = stringToSlug(tagName)
+      const postsForEachTag = postsByTag[tagName]
+
+      createPage({
+        path: `/tag/${slug}`,
+        component: singleTagTemplate,
+        context: {
+          posts: postsForEachTag,
+          tagName,
         },
       })
     })
---
+++ src/templates/tagPage.js
@@ -0,0 +1,26 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import Post from "../components/post/"
+
+function TagTemplate({ pageContext }) {
+  const { posts, tagName } = pageContext
+  return (
+    <Layout minimalist title={tagName}>
+      <div className="grid grid-cols-1 gap-4 my-20 md:gap-8 md:grid-cols-6">
+        {posts?.map(({ frontmatter }) => {
+          return (
+            <div
+              className="col-span-1 md:col-span-3 xl:col-span-2"
+              key={frontmatter.slug}
+            >
+              <Post {...frontmatter} showImg />
+            </div>
+          )
+        })}
+      </div>
+    </Layout>
+  )
+}
+
+export default TagTemplate
