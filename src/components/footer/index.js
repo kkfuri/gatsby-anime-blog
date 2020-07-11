a84826409517055dca239132566c4b182bfc23d7
Diego Kfuri <kkfuri@gmail.com>
2020-07-11 14:41:24 -0300

    fix: Remove link do github do footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -4,14 +4,14 @@ import { FaGithub } from "react-icons/fa"
 const Footer = () => (
   <footer className="w-full py-4 mx-auto bg-gray-200">
     <div className="container mx-auto text-right px-8">
-      <a
+      {/* <a
         href="https://github.com/kkfuri"
         target="_blank"
         rel="noreferrer noopener"
         aria-label="Ir para página do desenvolvedor no Github"
       >
         <FaGithub className="inline text-2xl text-gray-600 hover:text-primary duration-150" />
-      </a>
+      </a> */}
     </div>
   </footer>
 )
