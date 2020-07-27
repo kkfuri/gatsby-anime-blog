a6023ac90b45436dc55d008edbff175b714c3b26
Diego Kfuri <kkfuri@gmail.com>
2020-07-27 01:26:18 -0300

    feat: integrate share buttons with blog post link

--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -4,6 +4,7 @@ import { graphql } from "gatsby"
 import Img from "gatsby-image"
 import { MDXRenderer } from "gatsby-plugin-mdx"
 
+import ShareButtons from "../components/share-buttons"
 import Layout from "../components/layout"
 import NavigatePost from "../components/navigatePost"
 import Tag from "../components/tag"
@@ -29,22 +30,25 @@ function Template({ data, pageContext }) {
       keywords={tags}
       publishDate={publishDate}
     >
-      <div className="w-full max-w-4xl mx-auto md:mt-10">
-        <div className="flex flex-col justify-between mb-8">
-          <div className="flex flex-wrap items-center justify-between">
-            <div className="inline space-x-4">
+      <div className="w-full max-w-4xl mx-auto">
+        <div className="flex flex-col justify-between">
+          <div className="flex flex-col-reverse flex-wrap items-center justify-between md:flex-row">
+            <div className="flex flex-wrap">
               {tags?.map(tag => (
                 <Tag tag={tag} key={tag} />
               ))}
             </div>
-            <h5 className="text-xs tracking-widest uppercase font-body">
+            <h5 className="mx-auto text-xs tracking-widest uppercase md:mx-0 font-body">
               {dayjs(publishDate).format("DD . MMMM . YYYY")}
             </h5>
           </div>
+          <ShareButtons
+            link={data.site.siteMetadata.siteUrl.concat("/", slug)}
+          />
           <Img
             fadeIn
             fluid={heroImage?.fluid}
-            className="object-cover w-full h-64 mt-4 rounded md:mt-12 md:h-auto"
+            className="object-cover w-full h-64 my-4 rounded md:mb-8 md:h-auto"
             alt={`Imagem do post ${title}`}
           />
         </div>
@@ -79,6 +83,11 @@ function Template({ data, pageContext }) {
 
 export const query = graphql`
   query($pathSlug: String!) {
+    site {
+      siteMetadata {
+        siteUrl
+      }
+    }
     contentfulBlogPost(slug: { eq: $pathSlug }) {
       title
       tags
