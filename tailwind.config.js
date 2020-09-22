6f37deac1ae3ce5f57ba53217fde585233c25a0b
Diego Kfuri <kkfuri@gmail.com>
2020-09-22 20:54:56 -0300

    feat: add new feature with context and new colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,44 @@
 module.exports = {
   purge: ["./src/**/*.js", "./src/**/*.jsx", "./src/**/*.ts", "./src/**/*.tsx"],
   theme: {
+    typography: {
+      default: {
+        css: {
+          color: "var(--color-text-primary)",
+          h1: {
+            color: "var(--color-text-primary)",
+          },
+          h2: {
+            color: "var(--color-text-primary)",
+          },
+          h3: {
+            color: "var(--color-text-primary)",
+          },
+          h4: {
+            color: "var(--color-text-primary)",
+          },
+          h5: {
+            color: "var(--color-text-primary)",
+          },
+          h6: {
+            color: "var(--color-text-primary)",
+          },
+        },
+      },
+    },
     fontFamily: {
       display: "Alfa Slab One, Verdana",
       body: "Open Sans, Arial",
     },
     extend: {
-      colors: {
-        primary: {
-          default: "#038cfc",
-          dark: "#034d8a",
-          light: "",
-        },
+      backgroundColor: {
+        primary: "var(--color-bg-primary)",
+        secondary: "var(--color-bg-secondary)",
+      },
+      textColor: {
+        accent: "var(--color-text-accent)",
+        primary: "var(--color-text-primary)",
+        secondary: "var(--color-text-secondary)",
       },
     },
   },
